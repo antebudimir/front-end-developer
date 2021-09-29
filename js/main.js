355432b6e58d157098cb8a72b6a2ed6cb29e4877
Ante Budimir <abudimir85@gmail.com>
2021-09-29 19:31:56 +0200

    initial

--- js/main.js
+++ js/main.js
@@ -51,7 +51,6 @@
 	if (window.innerWidth < 1024) {
 		const hamburger = document.querySelector('.hamburger'),
 			menu = document.querySelector('.menu'),
-			menuIcon = document.querySelector('#menu-icon'),
 			body = document.querySelector('body');
 
 		hamburger.addEventListener('click', () => {
@@ -64,21 +63,11 @@
 					menu.style.animation = 'slideIn 400ms ease-in';
 				}, 1);
 
-				menuIcon.classList.remove('icon-menu');
-				menuIcon.classList.add('icon-x');
+				hamburger.classList.toggle('open');
 				body.style.overflow = 'hidden';
 			} else {
 				slideOut();
 			}
-		});
-
-		// Hide menu after clicking on anchored menu-item
-		const menuLinks = document.querySelectorAll('.menu-link');
-
-		menuLinks.forEach((link) => {
-			link.addEventListener('click', () => {
-				slideOut();
-			});
 		});
 
 		// Outside click
@@ -87,94 +76,252 @@
 		});
 
 		function slideOut() {
-			menu.style.animation = 'slideOut 350ms ease-in';
+			menu.style.animation = 'slideOut 450ms ease-in';
 			setTimeout(() => {
 				menu.style.display = 'none';
-			}, 300);
+			}, 400);
 
-			menuIcon.classList.remove('icon-x');
-			menuIcon.classList.add('icon-menu');
+			hamburger.classList.toggle('open');
 			body.style.overflow = 'auto';
 		}
 	}
 })();
 
-// Counter
-// Counter 1
-(function yearCounter() {
-	const observer = new IntersectionObserver(
-		function (entries) {
-			if (entries[0].isIntersecting === true) {
-				const numberOfYears = document.querySelector('.number-of-years');
-				let count = 0;
+// Intro section
+(function introCounter() {
+	const metrics = document.querySelector('#metrics'),
+		observer = new IntersectionObserver(
+			(entries) => {
+				entries.forEach((entry) => {
+					if (entry.isIntersecting === true) {
+						const years = document.querySelector('.years'),
+							projects = document.querySelector('.projects'),
+							clients = document.querySelector('.clients');
 
-				let counter = setInterval(() => {
-					count++;
-					numberOfYears.innerText = `${count}`;
-					if (count > 99) clearInterval(counter);
-				}, 20);
-			}
-		},
-		{ threshold: [1] },
-	);
-	observer.observe(document.querySelector('.years'));
+						counter(years, 10, 150);
+						counter(projects, 95, 35);
+						counter(clients, 35, 130);
+
+						function counter(subject, target, timing) {
+							let count = 0;
+
+							const counter = setInterval(() => {
+								count++;
+
+								subject.innerText = `${count}`;
+
+								if (count > target) {
+									clearInterval(counter);
+								}
+							}, timing);
+						}
+
+						observer.unobserve(metrics);
+					}
+				});
+			},
+			{ threshold: 0.3 },
+		);
+
+	observer.observe(metrics);
 })();
 
-// Counter 2
-// (function projectCounter() {
-// 	const observer = new IntersectionObserver(
-// 		function (entries) {
-// 			if (entries[0].isIntersecting === true) {
-// 				const numberOfProjects = document.querySelector('.number-of-projects');
-// 				let count = 1000;
+// Skills section
+(function skillsMeters() {
+	const meterBars = document.querySelector('#meter-bars'),
+		primaryFills = document.querySelectorAll('.primary-fill'),
+		secondaryFills = document.querySelectorAll('.secondary-fill'),
+		tertiaryFills = document.querySelectorAll('.tertiary-fill'),
+		fillsList = [primaryFills, secondaryFills, tertiaryFills],
+		allPercentages = [
+			'primary-percentage',
+			'secondary-percentage',
+			'tertiary-percentage',
+		],
+		observer = new IntersectionObserver(
+			(entries) => {
+				entries.forEach((entry) => {
+					if (entry.isIntersecting === true) {
+						// Filling bars with color
+						(function colorFiller() {
+							let i = 0;
 
-// 				let counter = setInterval(() => {
-// 					count += 1000;
-// 					numberOfProjects.innerText = `${count + '+'}`;
-// 					if (count > 199000) clearInterval(counter);
-// 				}, 10);
-// 			}
-// 		},
-// 		{ threshold: [1] },
-// 	);
+							fillsList.forEach((listItem, percentage) => {
+								percentage = allPercentages[i++];
 
-// 	observer.observe(document.querySelector('.years'));
-// })();
+								listItem.forEach((fill) => {
+									fill.classList.add(percentage);
+								});
+							});
+						})();
 
-// // scrollToTop button
-// (function scrollToTop() {
-// 	const scrollToTop = document.querySelector('.scrollButton');
+						// Counting percentages
+						(function percentageCounter() {
+							// 90%
+							const primaryPercentageNumbers = document.querySelectorAll(
+									'.primary-percentage-number',
+								),
+								// 80%
+								secondaryPercentageNumbers = document.querySelectorAll(
+									'.secondary-percentage-number',
+								),
+								// 50%
+								tertiaryPercentageNumbers = document.querySelectorAll(
+									'.tertiary-percentage-number',
+								);
 
-// 	window.addEventListener('scroll', () => {
-// 		if (
-// 			document.body.scrollTop > 1000 ||
-// 			document.documentElement.scrollTop > 1000
-// 		) {
-// 			scrollToTop.style.display = 'block';
-// 		} else {
-// 			scrollToTop.style.display = 'none';
-// 		}
-// 	});
+							primaryPercentageNumbers.forEach((number) => {
+								counter(number, 90, 32);
+							});
 
-// 	scrollToTop.addEventListener('click', () => {
-// 		document.body.scrollTop = 0;
-// 		document.documentElement.scrollTop = 0;
-// 	});
-// })();
+							secondaryPercentageNumbers.forEach((number) => {
+								counter(number, 80, 35);
+							});
+
+							tertiaryPercentageNumbers.forEach((number) => {
+								counter(number, 50, 56);
+							});
+
+							function counter(element, target, timing) {
+								let count = 0;
+
+								const counter = setInterval(() => {
+									element.innerText = `${count++}%`;
+
+									if (count > target) {
+										clearInterval(counter);
+									}
+								}, timing);
+							}
+						})();
+
+						observer.unobserve(meterBars);
+					}
+				});
+			},
+			{ threshold: 0.5 },
+		);
+
+	observer.observe(meterBars);
+})();
+
+// Create a box and group elements together for desktop layout
+(function groupingElements() {
+	function boxing(
+		containerName,
+		containerID,
+		elementOne,
+		elementTwo,
+		elementThree,
+		method,
+	) {
+		if (window.innerWidth > 1023) {
+			containerName = document.createElement('div');
+			containerName.setAttribute('id', `${containerID}`);
+
+			containerName.append(elementOne, elementTwo);
+
+			if (method === 'prepend') {
+				elementThree.prepend(containerName);
+			} else if (method === 'append') {
+				elementThree.append(containerName);
+			}
+		}
+	}
+
+	// HEADER
+	const headerSectionContainer = document.querySelector(
+			'#header .section-container',
+		),
+		primaryHeading = document.querySelector('.primary-heading'),
+		logoImg = document.querySelector('.logo-img');
+
+	boxing(
+		'headerBox',
+		'header-box',
+		logoImg,
+		primaryHeading,
+		headerSectionContainer,
+		'prepend',
+	);
+
+	// CONTACT
+	const map = document.querySelector('.map'),
+		contactForm = document.querySelector('#contact-form'),
+		contactSectionContainer = document.querySelector(
+			'#contact .section-container',
+		);
+
+	boxing(
+		'contactBox',
+		'contact-box',
+		map,
+		contactForm,
+		contactSectionContainer,
+		'append',
+	);
+})();
+
+// Sharing links
+(function sharingToggler() {
+	const toggle = document.querySelector('.toggle');
+
+	toggle.addEventListener('click', () => {
+		const iconContainers = document.querySelectorAll('.icon-container');
+
+		for (let i = 0; i < iconContainers.length; i++) {
+			iconContainers[i].classList.toggle('open');
+		}
+	});
+})();
+
+// scrollToTop button
+(function scrollToTop() {
+	const scrollToTop = document.querySelector('.scrollButton'),
+		sharingLinks = document.querySelector('#sharing-links');
+
+	window.addEventListener('scroll', () => {
+		// scroll button
+		if (
+			document.body.scrollTop > 1000 ||
+			document.documentElement.scrollTop > 1000
+		) {
+			scrollToTop.style.display = 'block';
+		} else {
+			scrollToTop.style.display = 'none';
+		}
+
+		// shared listener for sharing section too:
+		if (
+			document.body.scrollTop > 1500 ||
+			document.documentElement.scrollTop > 1500
+		) {
+			sharingLinks.style.display = 'flex';
+			sharingLinks.style.flexFlow = 'column nowrap';
+		} else {
+			sharingLinks.style.display = 'none';
+		}
+	});
+
+	scrollToTop.addEventListener('click', () => {
+		document.body.scrollTop = 0;
+		document.documentElement.scrollTop = 0;
+	});
+})();
 
 // Textarea auto resize
-// (function autoResize() {
-// 	const textArea = document.querySelector('[data-autoresize]'),
-// 		offset = textArea.offsetHeight - textArea.clientHeight;
+(function autoResize() {
+	const textArea = document.querySelector('[data-autoresize]'),
+		offset = textArea.offsetHeight - textArea.clientHeight;
 
-// 	textArea.addEventListener('input', (e) => {
-// 		e.target.style.height = 'auto';
-// 		e.target.style.height = e.target.scrollHeight + offset + 'px';
-// 	});
-// })();
+	textArea.addEventListener('input', (e) => {
+		e.target.style.height = 'auto';
+		e.target.style.height = e.target.scrollHeight + offset + 'px';
+	});
+})();
 
 // Date
-// (function getCurrentDate() {
-// 	const currentYear = document.querySelector('#currentYear');
-// 	currentYear.innerText = new Date().getFullYear();
-// })();
+(function getCurrentDate() {
+	const currentYear = document.querySelector('#currentYear');
+	currentYear.innerText = new Date().getFullYear();
+})();
